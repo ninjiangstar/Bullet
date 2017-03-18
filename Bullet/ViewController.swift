89175ea7e661072cb16e1365cd5b11849aad00f8
Andrew Jiang <andrewji@usc.edu>
2017-03-17 17:50:22 -0700

    Initial Commit

---
+++ Bullet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bullet
+//
+//  Created by Andrew Jiang on 3/17/17.
+//  Copyright © 2017 Andrew Jiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
